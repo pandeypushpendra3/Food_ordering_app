a780c344e32a2f53212c58bda49fd3047ac8919f
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2022-02-14 00:24:23 +0530

    navbar added

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {banner} from "../assets/banner.jpeg"
+export const Home = () => {
+  return (
+  <>
+      <div className="home">
+          <div className="headerContainer">
+              <img/>
+
+          </div>
+      </div>
+  </>
+  )
+}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,34 @@
-import {React} from 'react'
+import {React,useState} from 'react'
 import logo from '../assets/logo.png'
 import {Link} from "react-router-dom"
 import './Navbar.css'
+import ReorderIcon from "@mui/icons-material/Reorder"
 export const Navbar = () => {
+  const [showLinks,setShowLinks] = useState(false);
+  function Click(){
+    setShowLinks(!showLinks);
+  }
   return (
       <>
 <div className="navbar">
-<div className="leftside">
+<div className="leftside" id={showLinks?"open":"close"}>
 <img src={logo} alt="logo"/>
+<div className="hiddenLinks">
+<Link to="/" >Home</Link>
+<Link to="/menu">Menu</Link>
+<Link to="/about">About</Link> 
+<Link to="/contact">Contact</Link> 
+</div>
     </div>
 <div className="rightside">
 <Link to="/" >Home</Link>
 <Link to="/menu">Menu</Link>
 <Link to="/about">About</Link> 
-<Link to="/contact">Contact</Link>    
+<Link to="/contact">Contact</Link>
+<button onClick={Click}>
+<ReorderIcon/>  
+</button>
+  
 </div>
 
 </div>
