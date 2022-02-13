1f58df56412736c6734d931059a8b891c01ee80a
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2022-02-13 23:23:32 +0530

    assests added

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import {React} from 'react'
+
+export const Navbar = () => {
+  return (
+      <>
+<div className="navbar">
+<div className="leftside">
+    
+
+
+</div>
+<div className="rightside">
+    rightside
+</div>
+
+</div>
+      </>
+  
+  )
+}
