8700944be5c4c5026c87f8fd2cae446f13417ceb
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2022-02-13 23:59:53 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import {BrowserRouter as Router ,Switch,Route} from "react-router-dom"
 import {Navbar} from './Components/Navbar';
 function App() {
   return (
     <div className="App">
+     <Router>
      <Navbar/>
+     </Router>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,19 @@
 import {React} from 'react'
-
+import logo from '../assets/logo.png'
+import {Link} from "react-router-dom"
+import './Navbar.css'
 export const Navbar = () => {
   return (
       <>
 <div className="navbar">
 <div className="leftside">
-    
-
-
-</div>
+<img src={logo} alt="logo"/>
+    </div>
 <div className="rightside">
-    rightside
+<Link to="/" >Home</Link>
+<Link to="/menu">Menu</Link>
+<Link to="/about">About</Link> 
+<Link to="/contact">Contact</Link>    
 </div>
 
 </div>
